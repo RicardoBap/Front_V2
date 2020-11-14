dc77cbfef2bf3a6a2602ac423d845772608fb5cf
Ricardo <ricbapdevs@gmail.com>
2020-11-14 09:49:28 -0300

    Melhorando pagina de acesso-negado e colocando margem embaixo de 100 px

--- src/app/core/nao-autorizado/nao-autorizado.component.ts
+++ src/app/core/nao-autorizado/nao-autorizado.component.ts
@@ -4,12 +4,43 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-nao-autorizado',
   template: `
   <div class="container">
-    <div class="ui-g">
-      <h1 class="text-center">Acesso negado!</h1>
-    </div>
+
+    <div class="linha  row">
+     
+      <div class="p-grid  p-justify-center">
+        <h1><i class="pi pi-lock"></i></h1>
+      </div>
+
+      <div class="p-grid  p-justify-center">
+        <h2>Acesso negado</h2>
+      </div>
+
+      <div class="p-col-12">
+        <p>Desculpe, você não está autorizado a acessar a página que solicitou.<br/>
+        Se você acha que isso é um engano, entre em contato com a gente.</p>        
+      </div>
+
+      <div class="p-grid  p-justify-center">
+        <a pButton routerLink="/dashboard" label="Voltar para dashboard"> </a>
+      </div>     
+      
+    </div> 
+
   </div>
   `,
-  styles: [
+  styles: [`
+    i {
+      font-size: 2em;
+    }
+
+    .linha {
+      margin-top: 50px;
+    }
+
+    p {
+      text-align: center;
+    }  
+  `
   ]
 })
 export class NaoAutorizadoComponent implements OnInit {
@@ -20,3 +51,15 @@ export class NaoAutorizadoComponent implements OnInit {
   }
 
 }
+
+/*
+<div class="container">
+    <div class="ui-g">
+      <h1 class="text-center">Acesso negado!</h1>
+    </div>
+
+    <div>
+      <h1><i class="pi pi-lock" style="font-size: 3em"></i></h1>
+    </div>
+  </div>
+*/
