a587e5eeb58f8e45537d8db6097842ef140f10a4
Ricardo <ricbapdevs@gmail.com>
2021-01-01 11:13:15 -0300

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',   //'https://salvavidas-api.herokuapp.com',    'http://localhost:8080'
+  apiUrl: 'https://salvavidas-api.herokuapp.com',   //'https://salvavidas-api.herokuapp.com',    'http://localhost:8080'
 
-  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],  // salvavidas-api.herokuapp.com   'localhost:8080'
+  tokenWhitelistedDomains: [ new RegExp('salvavidas-api.herokuapp.com') ],  // salvavidas-api.herokuapp.com   'localhost:8080'
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
