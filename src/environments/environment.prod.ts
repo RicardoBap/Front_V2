a3d8b61a8e0f2c5815272d9a56d5153cf2c77682
RicardoBap <ric_bap@hotmail.com>
2022-05-24 17:20:14 -0300

    Mudando o tema

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://salvavidas-api.herokuapp.com',   //'https://salvavidas-api.herokuapp.com',    'http://localhost:8080'
 
-  tokenWhitelistedDomains: [ new RegExp('salvavidas-api.herokuapp.com') ],  // salvavidas-api.herokuapp.com   'localhost:8080'
-  tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
+  tokenWhitelistedDomains: [new RegExp('salvavidas-api.herokuapp.com')],  // salvavidas-api.herokuapp.com   'localhost:8080'
+  tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
 };
